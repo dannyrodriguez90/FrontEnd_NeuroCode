0ef962510f38627529a0f607de1b4bf7de51cb8f
dannyrodriguez90 <rmdanny889@gmail.com>
2025-05-08 12:50:46 -0600

    se agrega la estructura base del frontend junto con el navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../src/components/navbar/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
